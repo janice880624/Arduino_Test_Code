55afef0861bb1df33cb4942b633895bc2dda74dd
janice880624 <47550853+janice880624@users.noreply.github.com>
2020-12-22 22:12:20 +0800

    all

--- IR/IR.ino
+++ IR/IR.ino
@@ -2,7 +2,10 @@
 
 Servo myservo;
 
-int timer = 5000;
+int timer_wait = 5000;
+int timer_water = 6000;
+int timer_arm = 2000;
+int timer_bubble = 1500;
 
 // setting IR position
 int IR_Sensor_1 = 8;
@@ -14,7 +17,6 @@ int IR_2;
 int IR_3;
 
 // setting relay position
-
 int relay_1 = 7;
 int relay_2 = 4;
 
@@ -27,7 +29,7 @@ void setup(){
     pinMode(relay_2, OUTPUT);
     myservo.attach(9);
     myservo.write(0);
-    delay(timer);
+    delay(timer_wait);
 }
 
 void loop(){
@@ -45,43 +47,45 @@ void loop(){
     //-------------------------------------
 
     // condition 1st -> valve milktea
-    if (IR_1 == 0 && IR_2 == 1 && IR_3 == 1){
-        delay(2000); // add delay avoid start the arm before it arrives 
-        servo_bubble();
-        delay(timer);
+    if (IR_1 == 1 && IR_2 == 1 && IR_3 == 0){
+        delay(timer_arm); // add delay avoid start the arm before it arrives
+        water_1();
+        delay(timer_wait);
     }
 
     // condition 2ed -> valve blacktea
     if (IR_1 == 1 && IR_2 == 0 && IR_3 == 1){
-        delay(2000); // add delay avoid start the arm before it arrives
+        delay(timer_arm); // add delay avoid start the arm before it arrives
         water_2();
-        delay(timer);
+        delay(timer_wait);
     }
 
-    // condition 3rd -> servo
-    if (IR_1 == 1 && IR_2 == 1 && IR_3 == 0){
-        delay(2000); // add delay avoid start the arm before it arrives
-        water_1();
-        delay(timer);
+    // condition 3rd -> add bubble
+    if (IR_1 == 0 && IR_2 == 1 && IR_3 == 1){
+        delay(timer_arm); // add delay avoid start the arm before it arrives 
+        servo_bubble();
+        delay(timer_wait);
     }
+    
 }
 
-// add milktea redtea
+// add milktea
 int water_1(){
     digitalWrite(relay_1, HIGH); // add water
-    delay(6000);
+    delay(timer_water);
     digitalWrite(relay_1, LOW); // stop water
 }
 
+// add blacktea
 int water_2(){
     digitalWrite(relay_2, HIGH); // add water
-    delay(6000);
+    delay(timer_water);
     digitalWrite(relay_2, LOW); // stop water
 }
 
 // add bubble
 int servo_bubble(){
     myservo.write(90);
-    delay(1500);
+    delay(timer_bubble);
     myservo.write(0);
 }
